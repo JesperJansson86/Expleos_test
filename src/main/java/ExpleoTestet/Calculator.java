1a01bf0d90eaf5d09dae5cc03d392de8ecee6d1c
Jesper Jansson <jesper.jansson@yh.nackademin.se>
2021-03-27 14:51:45 +0100

    Skapat anagramfinder och linkFinder börjat lite smått på Calculator. Calculator är klar och verkar fungera fint. Dock återstår testerna att skapas.

--- src/main/java/ExpleoTestet/Calculator.java
+++ src/main/java/ExpleoTestet/Calculator.java
@@ -1,31 +1,179 @@
 package ExpleoTestet;
 
+import net.bytebuddy.implementation.bytecode.Throw;
 
-import java.awt.*;
 import java.util.ArrayList;
 
 public class Calculator {
-    public static void main(String[] args) {
-calci();
+
+
+    public static void main(String[] args) throws Exception {
+        Calculator c1 = new Calculator();
+        System.out.println(c1.calculatorLvl3("2+3*40"));
+        System.out.println(c1.calculatorLvl3("2*3+4"));
+        System.out.println(c1.calculatorLvl3("2/3+4-1"));
+        System.out.println(c1.calculatorLvl3("100+5*5/5*5"));
+
     }
 
-   static void calci() {
+    ArrayList convertStringToArrayList() {
         String mathstring = "2-3*4";
-       ArrayList expression = new ArrayList();
-       for (int i = 0; i < mathstring.length(); i++) {
-           expression.add(mathstring.charAt(i));
-       }
-
-
-    }
-
-    int recursiveLexParse(int first,ArrayList rest){
-if (Character.isDigit((char)rest.get(0)));{
-    first = first*10+(int)rest.get(0);
-            rest.remove(0);
+        ArrayList list = new ArrayList();
+        for (int i = 0; i < mathstring.length(); i++) {
+            list.add(mathstring.charAt(i));
         }
 
+        return list;
+    }
+
+    double calculatorLvl1(String input) throws Exception {
+        char operator = 'f';
+        int operatorlocation = 0;
+        for (int i = 0; i < input.length(); i++) {
+            if (!Character.isDigit(input.charAt(i))) {
+                operator = input.charAt(i);
+                operatorlocation = i;
+                break;
+            }
+
+        }
+        if (operator == 'f') throw new NumberFormatException("Felakting input");
+        double operand1 = Double.parseDouble(input.substring(0, operatorlocation));
+        double operand2 = Double.parseDouble(input.substring(operatorlocation + 1, input.length()));
+        if (operator == '+') return operand1 + operand2;
+        if (operator == '-') return operand1 - operand2;
+        if (operator == '*') return operand1 * operand2;
+        if (operator == '/') return operand1 / operand2;
+
+        else throw new Exception("Hur lyckades du komma hit?");
+    }
+
+    double evaluate(Double operand1, char operator, Double operand2) throws Exception {
+        if (operator == '+') return operand1 + operand2;
+        if (operator == '-') return operand1 - operand2;
+        if (operator == '*') return operand1 * operand2;
+        if (operator == '/') return operand1 / operand2;
+        else throw new Exception("Incorrect Operator");
+    }
+
+    double calculatorLvl2(String input) throws Exception {
+        ArrayList operators = new ArrayList();
+        ArrayList operands = new ArrayList();
+        //"2+30+4"
+        int whereDoesOperandStart = 0;
+        for (int i = 0; i < input.length(); i++) {
+            if (!Character.isDigit(input.charAt(i))) {
+
+                operands.add(Double.parseDouble(input.substring(whereDoesOperandStart, i)));
+                operators.add(input.charAt(i));
+                whereDoesOperandStart = i + 1;
+            }
 
 
+        }
+        operands.add(Double.parseDouble(input.substring(whereDoesOperandStart)));
+        double total = (double) operands.get(0);
+        for (var operator : operators
+        ) {
+            total = evaluate(total, (char) operator, (double) operands.get(1));
+            operands.remove(0);
+        }
+        return total;
+    }
+    double calculatorLvl3(String input) throws Exception {
+        ArrayList operators = new ArrayList();
+        ArrayList operatorlocations = new ArrayList();
+        ArrayList operands = new ArrayList();
+        ArrayList removeoperators = new ArrayList();
+        //"2+30+4"
+        int whereDoesOperandStart = 0;
+        for (int i = 0; i < input.length(); i++) {
+            if (!Character.isDigit(input.charAt(i))) {
+
+                operands.add(Double.parseDouble(input.substring(whereDoesOperandStart, i)));
+                operators.add(input.charAt(i));
+                operatorlocations.add(i);
+                whereDoesOperandStart = i + 1;
+            }
+
+
+        }
+        operands.add(Double.parseDouble(input.substring(whereDoesOperandStart)));
+        int index =0;
+        for (var operator : operators
+        ) {
+            if ((char)operator =='*'){
+                double temp = (double)operands.get(index)*(double)operands.get(index+1);
+                operands.remove(index);
+                operands.remove((index));
+                operands.add(index,temp);
+                removeoperators.add(index);
+            }else if ((char)operator =='/'){
+                double temp = (double)operands.get(index)/(double)operands.get(index+1);
+                operands.remove(index);
+                operands.remove((index));
+                operands.add(index,temp);
+                removeoperators.add(index);
+            }
+                else index++;
+        }
+        for (var usedoperator:removeoperators
+             ) {operators.remove((int)usedoperator);
+
+        }
+        removeoperators.clear();
+        index = 0;
+//        for (var operator : operators
+//        ) {
+//            if ((char)operator =='/'){
+//                double temp = (double)operands.get(index)/(double)operands.get(index+1);
+//                operands.remove(index);
+//                operands.remove((index));
+//                operands.add(index,temp);
+//                removeoperators.add(index);
+//            }
+//           else index++;
+//        }
+//        for (var usedoperator:removeoperators
+//        ) {operators.remove((int)usedoperator);
+//
+//        }
+//        removeoperators.clear();
+//        index = 0;
+        for (var operator : operators
+        ) {
+            if ((char)operator =='+'){
+                double temp = (double)operands.get(index)+(double)operands.get(index+1);
+                operands.remove(index);
+                operands.remove((index));
+                operands.add(index,temp);
+                removeoperators.add(index);
+            }else if ((char)operator =='-'){
+                double temp = (double)operands.get(index)-(double)operands.get(index+1);
+                operands.remove(index);
+                operands.remove((index));
+                operands.add(index,temp);
+                removeoperators.add(index);
+            }
+           else index++;
+        }
+//        for (var usedoperator:removeoperators
+//        ) {operators.remove((int)usedoperator);
+//
+//        }
+//        removeoperators.clear();
+//        index = 0;
+//        for (var operator : operators
+//        ) {
+//            if ((char)operator =='-'){
+//                double temp = (double)operands.get(index)-(double)operands.get(index+1);
+//                operands.remove(index);
+//                operands.remove((index));
+//                operands.add(index,temp);
+//                removeoperators.add(index);
+//            }
+//            else index++;
+//        }
+        return (double)operands.get(0);
     }
 }
